73f041149b413a2305efb7acfed12f099af17f6a
GamerSoft24 <haoyun.benjamin.chi@gmail.com>
2024-03-31 12:30:09 +0100

    Update windows360 aio.bat

--- BatchSoft/Windows/windows360 aio.bat
+++ BatchSoft/Windows/windows360 aio.bat
@@ -7,14 +7,13 @@ title Windows360 AIO
 echo Windows360 AIO/All-In-One:
 echo V1.26-beta. If action returns to main menu, that means the option is not implemented.
 echo (c) GamerSoftware Corporation. All rights reserved.
-echo If action returns to main menu, that means the option is not implemented.
-echo [1] System Infomations.
+echo [1] System Infomation.
 echo [2] Website Pinger.
 echo [3] Quit program.
 choice /c:123 /m "Choose an option: "
 IF ERRORLEVEL 3 GOTO QUIT
 IF ERRORLEVEL 2 GOTO WEBPING
-IF ERRORLEVEL 1 GOTO SYSTEMINFOS
+IF ERRORLEVEL 1 GOTO SYSTEMINFO
 :SYSTEMINFOS
 cls
 color a
