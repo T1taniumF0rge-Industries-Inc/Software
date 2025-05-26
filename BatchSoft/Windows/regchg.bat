b2b2b7aa0a6e14ca0bead412881823211254d182
gmaersot42 <162634737+GmaerSoft42@users.noreply.github.com>
2025-05-26 18:02:42 +0100

    so useful why is named as okmeque1 software

--- BatchSoft/Windows/regchg.bat
+++ BatchSoft/Windows/regchg.bat
@@ -6,7 +6,7 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-echo *** REGISTRY EDITOR (regchg.bat, running w/Admin Permissions) - (c) Okmeque1 Corporation and okmeque1\software ***
+echo *** REGISTRY EDITOR (regchg.bat, running w/Admin Permissions) - © Lan Internet Software***
 echo This program will make it easy for you to disable certain annoying Windows Features.
 echo WARNING! The uninstall Edge feature is completely functional but may throw errors due to the way the program deletes it. This program deletes Edge by forcibly removing all Edge-related functions, even if they don't exist.
 echo For users that use applications that rely on Microsoft Edge WebView2, it is highly recommended that you do not uninstall Microsoft Edge as applications that rely on WebView2 also rely on core functions of Microsoft Edge.
