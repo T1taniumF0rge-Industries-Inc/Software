126754405439f0a4c207bb105b1589d70cc8922e
Okmeque1 <okmeque1@gmail.com>
2025-08-07 19:38:11 +0200

    stupid tkinter error handling

--- PySoft/Editors/CMDEdit.py
+++ PySoft/Editors/CMDEdit.py
@@ -2,6 +2,7 @@ import msvcrt, os
 global text
 text = ""
 file_name = ""
+encoder = 'utf-8'
 def clear():
     if os.name == 'nt':
         os.system("cls")
@@ -33,7 +34,7 @@ while True:
         clear()
         print("CMDEdit Text Editor - © Okmeque1 Software")
         print(text)
-        print("CTRL-S = Save current loaded file, CTRL-E = Save as, CTRL-X = Load, CTRL-F = Find, CTRL-G = Find/Replace, CTRL-Z = Exit")
+        print("CTRL-S = Save current loaded file, CTRL-E = Save as, CTRL-X = Load, CTRL-F = Find, CTRL-G = Find/Replace, CTRL-W = Configure Encoder, CTRL-Z = Exit")
         character = msvcrt.getwch()
         if character == '\x08' and len(text) > 0: #Backspace and text delete
             text = text[:-1]
@@ -41,13 +42,22 @@ while True:
             text += '\n'
         elif character == '\x05': # CTRL-E, use for saving in a different file
             file_name = input("\nPlease enter a file name (A:\directory\subdirectory if on Windows, /dev/sda/folder on most other systems.). Full path must be specified if the file is not in the same directory as this program: ")
-            with open(file_name, "w") as save:
+            with open(file_name, "w", encoding=encoder) as save:
                 save.write(text)
             input("File saved. Press ENTER to continue...")
         elif character == "\x13": # CTRL-S, save to current file if loaded
-            with open(file_name, "w") as save:
+            with open(file_name, "w", encoding=encoder) as save:
                 save.write(text)
             input("File saved. Press ENTER to continue.")
+        elif character == "\x17":
+            encoder = input("Choose encoder:\n[1] UTF-8 (most common for all systems)\n[2] ANSI (most common in Windows Pre-Vista)\n[3] Custom\nSelect: ")
+            if encoder == "1":
+                encoder = 'utf-8'
+            elif encoder == "2":
+                encoder = 'ansi'
+            else:
+                encoder = input("Enter name of encoder: ")
+            input("Encoder configured. Press ENTER to continue...")
         elif character == '\x18': # CTRL-X, use for loading
             file_name = input("\nPlease enter a valid file name (A:\directory\subdirectory if on Windows, /dev/sda/folder on most other systems.). Full path must be specified if the file is not in the same directory as this program: ")
             with open(file_name, "r") as load:
@@ -63,12 +73,20 @@ while True:
             exit()
         else:
             text += character
+    except LookupError as e:
+        print(f"STOP: 1E/18\nCannot load the specified encoder. Make sure that the encoder is valid and that you typed it correctly. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nDetails: {e}")
+        input("Press ENTER to continue...")
+        continue
+    except UnicodeDecodeError as e:
+        print(f"STOP: 1E/10\nEncoder selected failed to parse the specified file. Try using a different encoder (if you were using UTF-8, try ANSI) and try again. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nDetails: {e}")
+        input("Press ENTER to continue...")
+        continue
     except FileNotFoundError as e: # Error codes are part of the GitHub GamerSoft24/Software PySoft errors chart. 
         print(f"STOP: 6510B\nCannot find the specified file. Make sure that the file you specified is a valid file and that it is spelled correctly.\nDetails: {e}")
         input("Press ENTER to continue...")
         continue
     except FileExistsError as e:
-        print(f"STOP: 6510A\nThe file that you specified is conflicting with another file. Delete the conflicting file, choose a different name for your file or rename the conflicting file.\nDetails: {e}")
+        print(f"STOP: 6510A\nThe file that you specified is conflicting with another file. Delete the conflicting file, choose a different name for your file or rename the conflicting file. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nDetails: {e}")
         input("Press ENTER to continue...")
         continue
     except Exception as e:
--- PySoft/Editors/gui-text editor.py
+++ PySoft/Editors/gui-text editor.py
@@ -1,28 +1,63 @@
 try:
     import tkinter as tk
     from tkinter import filedialog, messagebox, simpledialog
-
+    global encoder
     def new_file():
         text.delete("1.0", tk.END)
 
     def open_file():
-            file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
-            if file_path: # Check if user actually put a file
-                with open(file_path, "r") as file:
-                    content = file.read()
-                text.delete("1.0", tk.END)
-                text.insert("1.0", content)
-
+            try:
+                global encoder
+                file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+                if file_path: # Check if user actually put a file
+                    with open(file_path, "r",encoding=encoder) as file:
+                        content = file.read()
+                    text.delete("1.0", tk.END)
+                    text.insert("1.0", content)
+            except LookupError as e:
+                x = messagebox.showerror("GUI Text Editor",f"Could not load the specified encoder. Make sure that it is valid and that you typed it correctly. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError code: 1E/18\nDetails: {e}")
+            except UnicodeDecodeError as e:
+                x = messagebox.showerror("GUI Text Editor",f"Encoder failed to parse the specified file. Try using a different encoder (if you were using UTF-8, try ANSI) and try again. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError Code: 1E/10\nDetails: {e}")
+            except FileExistsError as e: # Error codes can be found at https://github.com/GamerSoft24/Software/blob/Main/PySoft/Errors%20chart.md
+                x = messagebox.showerror("GUI Text Editor",f"The file that you specified is conflicting with another file. Delete or rename the conflicting file, or choose an alternative file name. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError Code: 6510A\nDetails: {e}")
+            except FileNotFoundError as e:
+                x = messagebox.showerror("GUI Text Editor",f"The file that you specified is not valid. Make sure the file exists and that you typed it correctly.\nError Code: 6510B\nDetails: {e}")
+            except PermissionError as e:
+                x = messagebox.showerror("GUI Text Editor",f"Access violation in file. Make sure you can use the file, then try again.\nError Code: 0211\nDetails: {e}")
+            except Exception as e:
+                x = messagebox.showerror("GUI Text Editor",f"Unhandled Exception 0x0000024066EF3E20 has occured in this program. Review the GitHub GamerSoft24/Software PySoft error chart and the Python manual for more info.\nError Code: 770A\nDetails: {e}")
     def save_file():
-        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
-        if file_path: # Same thing checking
-            with open(file_path, "w") as file:
-                file.write(text.get("1.0", tk.END))
-            messagebox.showinfo("Info", "File saved successfully!")
-        
+        try:
+            global encoder
+            file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+            if file_path: # Same thing checking
+                with open(file_path, "w",encoding=encoder) as file:
+                    file.write(text.get("1.0", tk.END))
+                messagebox.showinfo("Info", "File saved successfully!")
+        except LookupError as e:
+                x = messagebox.showerror("GUI Text Editor",f"Could not load the specified encoder. Make sure that it is valid and that you typed it correctly. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError code: 1E/18\nDetails: {e}")
+        except UnicodeDecodeError as e:
+            x = messagebox.showerror("GUI Text Editor",f"Encoder failed to parse the specified file. Try using a different encoder (if you were using UTF-8, try ANSI) and try again. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError Code: 1E/10\nDetails: {e}")
+        except FileExistsError as e: # Error codes can be found at https://github.com/GamerSoft24/Software/blob/Main/PySoft/Errors%20chart.md
+            x = messagebox.showerror("GUI Text Editor",f"The file that you specified is conflicting with another file. Delete or rename the conflicting file, or choose an alternative file name. Note that if you were doing a save operation, the last changes were not saved to the file but are still in the editor, and you will have to re-save the file.\nError Code: 6510A\nDetails: {e}")
+        except FileNotFoundError as e:
+            x = messagebox.showerror("GUI Text Editor",f"The file that you specified is not valid. Make sure the file exists and that you typed it correctly.\nError Code: 6510B\nDetails: {e}")
+        except PermissionError as e:
+            x = messagebox.showerror("GUI Text Editor",f"Access violation in file. Make sure you can use the file, then try again.\nError Code: 0211\nDetails: {e}")
+        except Exception as e:
+            x = messagebox.showerror("GUI Text Editor",f"Unhandled Exception 0x0000024066EF3E20 has occured in this program. Review the GitHub GamerSoft24/Software PySoft error chart and the Python manual for more info.\nError Code: 770A\nDetails: {e}")
+
     def about():
         messagebox.showinfo("About", "Python Text Editor\nCreated by GamerSoft24.\n© GamerSoftware Corporation™. All rights reserved.\n© Find/Replace feature by Okmeque1 Software")
-
+    def configure_encoder():
+        global encoder
+        encoder = simpledialog.askstring("Encoder configuration","Choose an encoder:\n[1] UTF-8 (most common in modern systems)\n[2] ANSI (most common in Windows Pre-Vista)\n[3] Custom")
+        if encoder == "1":
+            encoder = 'utf-8'
+        elif encoder == "2":
+            encoder = 'ansi'
+        else:
+            encoder = simpledialog.askstring("Custom Encoder configuration","Enter name of encoder: ")
     def find_replace(parameter=False):
         tofind = simpledialog.askstring("Find","Enter value to find in file: ")
         toreplace = ''
@@ -52,7 +87,7 @@ try:
             text.insert(tk.END, window)
     root = tk.Tk()
     root.title("Python Text Editor")
-
+    encoder = 'utf-8'
     menu = tk.Menu(root)
     root.config(menu=menu)
 
@@ -68,6 +103,7 @@ try:
     menu.add_cascade(label="Edit", menu=edit_menu)
     edit_menu.add_command(label="Find ", command=find_replace)
     edit_menu.add_command(label="Find/Replace", command=lambda: find_replace(parameter=True))
+    edit_menu.add_command(label="Configure encoder", command=configure_encoder)
     help_menu = tk.Menu(menu)
     menu.add_cascade(label="Help", menu=help_menu)
     help_menu.add_command(label="About", command=about)
@@ -78,29 +114,6 @@ try:
     root.geometry("800x600")
     root.resizable(width=True, height=True)
     root.mainloop()
-except FileExistsError as e: # Error codes can be found at https://github.com/GamerSoft24/Software/blob/Main/PySoft/Errors%20chart.md
-    x = messagebox.showerror("GUI Text Editor",f"The file that you specified is conflicting with another file. Delete or rename the conflicting file, or choose an alternative file name.\nError Code: 6510A\nDetails: {e}")
-except FileNotFoundError as e:
-    x = messagebox.showerror("GUI Text Editor",f"The file that you specified is not valid. Make sure the file exists and that you typed it correctly.\nError Code: 6510B\nDetails: {e}")
-except PermissionError as e:
-    x = messagebox.showerror("GUI Text Editor",f"Access violation in file. Make sure you can use the file, then try again.\nError Code: 0211\nDetails: {e}")
-
-except IOError as e:
-    x = messagebox.showerror("GUI Text Editor",f"I/O error. You have unplugged a device or a device on the host is malfunctioning. Program will now exit\nError Code: 0272\nDetails: {e}")
-    exit()
-except OSError as e:
-    x = messagebox.showerror("GUI Text Editor",f"A system error has occured. Check for disk corruption, faulty/loosely connected hardware. Program will now exit\nError Code: 0271\nDetails: {e}")
-    exit()
-except ValueError as e:
-    x = messagebox.showerror("GUI Text Editor",f"You have entered a wrong value or this program has been tampered with. When asked for a value, enter the CORRECT values that are demanded.\nError Code: 0211\nDetails: {e}")
-except KeyboardInterrupt:
-    print("LOG: User has chosen to exit. Exiting... (0270)")
-    exit()
-except EOFError:
-    print("LOG: User has chosen to exit. Exiting... (0250)")
-    exit()
-except SystemExit:
-    exit()
 except Exception as e:
-    x = messagebox.showerror("GUI Text Editor",f"Unhandled Exception at 0x0000024066EF3E20 has occured in this program. Review the GitHub GamerSoft24/Software PySoft error chart and the Python manual for more info.\nError Code: 770A\nDetails: {e}")
+    x = messagebox.showerror("GUI Text Editor",f"Unhandled Exception 0x0000024066EF3E20 has occured in this program. Review the GitHub GamerSoft24/Software PySoft error chart and the Python manual for more info.\nError Code: 770A\nDetails: {e}")
 
